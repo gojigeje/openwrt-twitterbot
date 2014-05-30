6871ccb77fcf9ffdaf238ec51e9f39bbe13c7ff6
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-05-30 21:57:26 +0700

    main script

---
+++ tweet.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# -------------------------------------------------------------------------
+
+# change directory to script's directory
+# cd /root/script/twit
+
+setup() {
+  hari=$(date +%u)
+  tanggal=$(date +%d)
+  bulan=$(date +%m)
+  tahun=$(date +%Y)
+  jam=$(date +%H)
+  aksi_array=()
+
+  # load plugin
+  for f in plugin/*; do
+     . $f
+     # echo "$f"
+  done
+
+  # exit 1
+  if [[ -z "$1" ]]; then
+    aksi_num=${#aksi_array[*]}
+    aksi=${aksi_array[$((RANDOM%aksi_num))]}
+    # make sure aksi not same like before
+    if [[ -f "temp/$aksi.last" ]]; then
+      setup
+    else
+      rm temp/*.last
+      touch "temp/$aksi.last"
+    fi
+  else
+    aksi="$1"
+    rm temp/*.last
+    touch "temp/$aksi.last"
+  fi
+}
+getJam() {
+  # getjam=$(date +'%H:%M:%S')
+  getjam=$(date +'%H:%M')
+}
+
+post_tweet() {
+  if [[ ! -z "$tweet_status" ]]; then
+    getJam
+    echo "[ttytter] : $tweet_status ~ $getjam"
+    # echo "$tweet_status ~ $getjam" | ttytter -status=-
+  else
+    echo "ERROR_STATUS_KOSONG: $aksi"
+  fi
+}
+
+if [[ -z "$1" ]]; then
+  setup
+  "$aksi"_main
+  post_tweet
+
+  # once more when action is greeting
+  if [[ $aksi = "twit_greeting" ]]; then
+    setup
+    "$aksi"_main
+    post_tweet  
+  fi
+
+else
+  setup "$1"
+  "$aksi"_main "${@:2}"
+  post_tweet
+fi
