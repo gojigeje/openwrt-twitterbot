3dde29c2872c756d23e3ddbee7db225b165c0163
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-06-06 16:30:13 +0700

    fix: not choosing target by random, but sequentially

--- plugin/ejek.sh
+++ plugin/ejek.sh
@@ -3,113 +3,111 @@
 aksi_array+=("twit_ejek")
 
 twit_ejek_pilihtarget() {
-  target_num=${#target_array[*]}
-  target=${target_array[$((RANDOM%target_num))]}
+  ejek_num=${#target_array[*]}
+  ejek_num_max=$(( ejek_num - 1 ))
 
-  # pasitkan ejekan sebelumnya bukan untuk target ini
+  # cek entri di config
   entri="plugin_ejek_last"
   if [[ "$plugin_ejek_last" = "" ]]; then
     if grep -q "$entri" "plugin/plugin.conf" ; then
-      # echo "entri ada tapi kosong, replace"
-      sed -i 's/'$entri'*.*/'$entri'="'$target'"/g' "plugin/plugin.conf"
+      sed -i 's/'$entri'*.*/'$entri'="0"/g' "plugin/plugin.conf"
     else
-      # echo "entri ga ada, insert"
-      echo "$entri=\"$target\"" >> "plugin/plugin.conf"
+      echo "$entri=\"0\"" >> "plugin/plugin.conf"
     fi
-
+    urutan="0"
+    
   else # cek isinya
-    if [[ "$target" != "$plugin_ejek_last" ]]; then
-      # echo "isinya nggak sama, replace"
-      sed -i 's/'$entri'*.*/'$entri'="'$target'"/g' "plugin/plugin.conf"
+    if [[ "$plugin_ejek_last" -eq "$ejek_num_max" ]]; then
+      urutan="0"
     else
-      # echo "target sama, pilih lagi [$target]"
-      twit_ejek_pilihtarget
+      urutan=$(( plugin_ejek_last + 1 ))
     fi
+    sed -i 's/'$entri'*.*/'$entri'='\"$urutan\"'/g' "plugin/plugin.conf"
+
   fi
+
+  target=${target_array[$urutan]}
 }
 
 # main method
 twit_ejek_main() {
   target_array=(
-      "@afandie5"
-      "@aftaonline"
-      "@AgungTarecha"
-      "@Ampyunk"
-      "@BimaFikry"
-      "@blu3f4lc0n"
-      # "@dan_guna"
-      # "@didikz"
-      # "@dimperm"
-      "@ekosuhartono_"
-      "@Galih_Gundul"
-      "@gojigeje"
-      "@mang_panga"
-      # "@Marsha_Say"
       "@mfthl"
-      "@muvidm"
-      "@MuzanRizqy"
-      "@pogal_666"
-      # "@putrinawa"
-      # "@Ressaniaa"
-      "@RezaTedjoe"
-      "@rikza7x"
-      "@TegarSwasono"
-      # "@ulinzulfa"
       "@yudhasatyaap"
+      "@afandie5"
+      "@Ressaniaa"
+      "@mang_panga"
+      "@pogal_666"
       "@yunushadi"
+      "@MuzanRizqy"
+      "@AgungTarecha"
+      "@Marsha_Say"
+      "@blu3f4lc0n"
+      "@Ampyunk"
+      "@gojigeje"
+      "@BimaFikry"
+      "@EKOSUHARTONOcom"
+      "@ulinzulfa"
+      "@muvidm"
+      "@rikza7x"
+      "@RezaTedjoe"
+      "@TegarSwasono"
+      "@aftaonline"
+      "@putrinawa"
     )
   twit_ejek_pilihtarget
 
   peringatan_array=(
-      "ane baru tau"
-      "ane berani jamin"
-      "ane berani sumpah"
-      "ane udah buktiin"
-      "ane udah lihat sendiri"
-      "ati-ati rek"
-      "ati-ati"
-      "harap waspada"
-      "hati-hati rek"
-      "hati-hati"
-      "rek ati-ati"
-      "rek hati-hati"
-      "rek waspadalah"
-      "tolong waspada"
-      "waspadalah rek"
-      "waspadalah"
+      "ane baru tau.."
+      "ane berani jamin.."
+      "ane berani sumpah.."
+      "ane udah buktiin.."
+      "ane udah lihat sendiri.."
+      "ati-ati rek.."
+      "ati-ati.."
+      "harap waspada.."
+      "hati-hati rek.."
+      "hati-hati.."
+      "rek ati-ati.."
+      "rek hati-hati.."
+      "rek waspadalah.."
+      "hot newsss.."
+      "waspadalah rek.."
+      "waspadalah.."
     )
   peringatan_num=${#peringatan_array[*]}
   peringatan=${peringatan_array[$((RANDOM%peringatan_num))]}
 
   ejekan_array=(
-      "ada gejala maho"
-      "banyak makannya"
-      "ga pernah gosok gigi"
-      "bulu keteknya panjang"
-      "ga pernah mandi"
-      "kalo beol selalu lupa disiram"
-      "kalo deket cewek suka lirak-lirik"
-      "kalo malam suka ngep*t"
-      "kalo marah suka banting kursi"
-      "kalo tidur suka ngorok"
-      "kentutnya super bau"
-      "lagi bokek, udah 3 hari cuma makan mie instan"
-      "maho sejati #terbukti"
-      "super maho"
-      "menganggap dirinya superhero"
-      "obatnya mau habis"
-      "spesies langka, hampir punah.. tolong dijaga"
-      "suka kentut sembarangan"
-      "suka kentut"
-      "suka lama-lama di kamar mandi"
-      "suka lirak-lirik cewek"
-      "suka menggigit"
-      "suka ngamuk-ngamuk kalo lagi marah"
-      "suka ngupil"
-      "udah 3 hari ga mandi"
-      "udelnya bodong"
-      "upilnya banyak"
-      "kalo tidur ngileran"
+      "ada gejala-gejala maho.."
+      "super banyak makannya, 3 piring bisa abis.."
+      "ga pernah gosok gigi.."
+      "bulu keteknya panjang-panjang.."
+      "ga pernah mandi.. mandi cuma setaun sekali pas lebaran.."
+      "kalo beol selalu lupa disiram.. gue korbannya.."
+      "kalo deket cewek matanya suka lirak-lirik.."
+      "kalo malam suka ngep*t.. ane yang biasa jagain lilinnya.."
+      "kalo marah suka banting-banting apa aja yang ada di jangkauan dia.."
+      "kalo tidur suka ngorok, dan ngoroknya ampunnn.. kerasnya kayak pake speaker musholla.."
+      "kentutnya super bau.. sekali kentut orang serumah KO semua.."
+      "lagi bokek, udah 3 hari cuma makan mie instan.."
+      "maho sejati #terbukti.. "
+      "super maho.. dan dia nganggap super itu kayak di superhero gitu.. dan dia bangga.."
+      "menganggap dirinya superhero.. suka loncat + lari-lari pake baju berjubah gitu.."
+      "obatnya kayaknya mau habis.. sandal aku baru habis dimakan sama dia.."
+      "spesies langka, hampir punah.. baiknya dijaga.. masukin cagar alam.."
+      "suka kentut sembarangan.. sumpah ane malu pas dia kentut di angkot tadi.."
+      "suka kentut.. dan kentutnya superrrrr bau.."
+      "suka lama-lama di kamar mandi.. ga tau ngapain di dalam.. tidur kayaknya.."
+      "suka lirak-lirik cewek umur 60 tahun ke atas.. selamatkan nenek kamu.."
+      "suka menggigit.. bantal, kasur, sendal, tembok semua digigit ama dia.."
+      "suka ngamuk-ngamuk kalo lagi marah.."
+      "suka ngupil.. ni sekarang lagi asyik ngupil dia.."
+      "kalo ngupil, upilnya suka ditaroh sembarangan.."
+      "udah 3 hari ga mandi.. sampe banyak lalat berputar-putar di atas kepalanya.."
+      "udelnya bodong.. kayak sumur.."
+      "upilnya banyak.."
+      "kalo tidur ngileran.."
     )
   ejekan_num=${#ejekan_array[*]}
   ejekan=${ejekan_array[$((RANDOM%ejekan_num))]}  
--- plugin/gombal.sh
+++ plugin/gombal.sh
@@ -4,51 +4,79 @@ aksi_array+=("twit_gombal")
 
 twit_gombal_pilihtarget() {
   target_num=${#target_array[*]}
-  target=${target_array[$((RANDOM%target_num))]}
+  target_num_max=$(( target_num - 1 ))
 
-  # pasitkan gombalan sebelumnya bukan untuk target ini
+  # cek entri di config
   entri="plugin_gombal_last"
   if [[ "$plugin_gombal_last" = "" ]]; then
     if grep -q "$entri" "plugin/plugin.conf" ; then
-      # echo "entri ada tapi kosong, replace"
-      sed -i 's/'$entri'*.*/'$entri'="'$target'"/g' "plugin/plugin.conf"
+      sed -i 's/'$entri'*.*/'$entri'="0"/g' "plugin/plugin.conf"
     else
-      # echo "entri ga ada, insert"
-      echo "$entri=\"$target\"" >> "plugin/plugin.conf"
+      echo "$entri=\"0\"" >> "plugin/plugin.conf"
     fi
-
+    urutan="0"
+    
   else # cek isinya
-    if [[ "$target" != "$plugin_gombal_last" ]]; then
-      # echo "isinya nggak sama, replace"
-      sed -i 's/'$entri'*.*/'$entri'="'$target'"/g' "plugin/plugin.conf"
+    if [[ "$plugin_gombal_last" -eq "$target_num_max" ]]; then
+      urutan="0"
     else
-      # echo "target sama, pilih lagi [$target]"
-      twit_gombal_pilihtarget
+      urutan=$(( plugin_gombal_last + 1 ))
     fi
+    sed -i 's/'$entri'*.*/'$entri'='\"$urutan\"'/g' "plugin/plugin.conf"
+
   fi
+
+  target=${target_array[$urutan]}
+}
+
+twit_gombal_pilihgombalan() {
+  gombalan_num=${#gombalan_array[*]}
+  gombalan_num_max=$(( gombalan_num - 1 ))
+
+  # cek entri di config
+  entri="plugin_gombalan_last"
+  if [[ "$plugin_gombalan_last" = "" ]]; then
+    if grep -q "$entri" "plugin/plugin.conf" ; then
+      sed -i 's/'$entri'*.*/'$entri'="0"/g' "plugin/plugin.conf"
+    else
+      echo "$entri=\"0\"" >> "plugin/plugin.conf"
+    fi
+    urutan="0"
+    
+  else # cek isinya
+    if [[ "$plugin_gombalan_last" -eq "$gombalan_num_max" ]]; then
+      urutan="0"
+    else
+      urutan=$(( plugin_gombalan_last + 1 ))
+    fi
+    sed -i 's/'$entri'*.*/'$entri'='\"$urutan\"'/g' "plugin/plugin.conf"
+
+  fi
+
+  gombalan=${gombalan_array[$urutan]}
 }
 
 # main method
 twit_gombal_main() {
   target_array=(
-      "@AllyciaAP"
-      "@AnandaRPD"
-      "@AnindaRPD"
-      "@chen9_"
-      "@Chindy_horor"
-      "@cipluxx"
-      "@d34ff4"
+      "@sarri__"
       "@hanazawaqurin"
-      "@jessy_zy"
-      "@Marsha_Say"
+      "@d34ff4"
+      "@Zheezayn"
+      "@RomsZhu"
       "@putrinawa"
+      "@Chindy_horor"
+      "@AllyciaAP"
+      "@ristina55"
       "@putrinawa"
       "@Ressaniaa"
-      "@ristina55"
-      "@RomsZhu"
-      "@sarri__"
+      "@cipluxx"
+      "@AnindaRPD"
       "@ulinzulfa"
-      "@Zheezayn"
+      "@AnandaRPD"
+      "@jessy_zy"
+      "@chen9_"
+      "@Marsha_Say"
     )
   twit_gombal_pilihtarget
 
@@ -56,73 +84,71 @@ twit_gombal_main() {
   # http://muhamadsahlan.blogspot.com/2012/02/kata-kata-gombalan-galau-di-twitter-ii.html
   # http://blog-penerang.blogspot.com/2013/03/kata-kata-gombal-gokil-terbaru-2013.html
   gombalan_array=(
-      "Kamu itu udah bikin aku nyesel, bikin aku nyesel banget kenapa gak kenal kamu dari dulu."
-      "Menurut km,lebih susah mana pelajaran matematika sama fisika? | menurut aku sih lebih susah lupain mantan | #___#"
+      "Kamu itu udah bikin aku nyesel, bikin aku nyesel banget kenapa gak kenal kamu dari dulu. :D"
+      "Menurut km,lebih susah mana pelajaran matematika sama fisika? | menurut aku sih lebih susah lupain kamu | #___#"
       "Punya deterjen pemutih ga? | buat apa? | aku mau mencuci cinta suci kita yg telah kau nodai (-̩-̩̩-̩u-̩-̩̩-̩)"
-      "Jujur aja gue ini bkn cowo baik-baik. Suka mencuri. Apalagi mencuri cintamu."
-      "Boleh pinjem meteran? | Buat apa? | Aku mau mengukur kedalaman cintaku padamu.~"
+      "Jujur aja gue ini bkn cowo baik-baik. Suka mencuri. Apalagi mencuri cintamu. :D"
+      "Boleh pinjem meteran? | Buat apa? | Aku mau mengukur kedalaman cintaku padamu. :D"
       "Nitip beliin racun tikus donk | buat apa? | buat ngeracun pacar km -____-"
-      "Woy maling,maling,maling! Ada yg mau maling hatiku ni :D"
-      "Masa lalu km sama mantan mungkin memang indah, tp masa depan km bersamaku akan jauh lebih indah, nikah yuuk"
-      "Rhoma Irama itu Raja Dangdut, Michael jackson itu Raja Pop, kalau aku itu RAJA dihatimu"
-      "Apa beda'y km sama orang utan? | ga tau | kalo orang utan suka ngegantung diatas pohon, klo km suka ngegantung cinta aku"
+      "Woy maling,maling,maling! Ada yg mau maling hatiku ni :D :D"
+      "Masa lalu km sama mantan mungkin memang indah, tp masa depan km bersamaku akan jauh lebih indah, pacaran yuuk :D"
+      "Rhoma Irama itu Raja Dangdut, Michael jackson itu Raja Pop, kalau aku itu RAJA dihatimu :D"
+      "Apa beda'y km sama orang utan? | g tau | kalo orang utan suka ngegantung di pohon, klo km suka ngegantung cinta aku :D"
       "Punya kain kafan gak? | buat apa? | aku mau membungkus perasaanku yg sudah kau bunuh (-̩-̩̩-̩u-̩-̩̩-̩)"
-      "Cowo:Kamu tau nggak tadi malem aku mimpi nyasar kemana? Cewe:Kemana?? Cowo:Kehatimu!"
-      "Bapak kamu koruptor ya ? | kok tau ? | karna kamu telah mengkorupsi cinta dan kepercayaanku"
-      "Kita main rampok-rampokan yuk? Kyk gmna? Kamu curi hatiku, aku curi hatimu"
-      "Pinjem modem boleh? | Mau ngapain? | Mau nyari file hati kamu dan mau Upload rasa sayang aku buat kamu."
+      "Cowo:Kamu tau nggak tadi malem aku mimpi nyasar kemana? Cewe:Kemana?? Cowo:Kehatimu! :D"
+      "Bapak kamu koruptor ya ? | kok tau ? | karna kamu telah mengkorupsi cinta dan kepercayaanku :D"
+      "Kita main rampok-rampokan yuk? Kyk gmna? Kamu curi hatiku, aku curi hatimu :D"
+      "Pinjem modem boleh? | Mau ngapain? | Mau nyari file hati kamu dan mau Upload rasa sayang aku buat kamu. :D"
       "Waah km emang berbakat deh | berbakat apa? | berbakat jadi Qeong Racuuunnnn (¬.¬)ƪ(˘ε˘“)"
-      "Aku kayaknya gagal jadi penulis deh Emgnya knapa? Soalnya daritadi yg aku tulis cuma nama kamu aja"
-      "Hai mantan, kamu lagi belajar yah? | iya kok tau? | soalnya aku juga sama nih lagi belajar lupain km"
-      "Jalan-jalan kesurabaya tak lupa ke suramadu. Buat kau yang disana.. I miss you :)"
-      "Neng,klo kmu jd gula. Abang gk mau jd semutnya | Kok gtu bang? | soalnya abang gk mau m'habiskan kmanisan wjahmu.. | *senyum*"
-      "Neng, tolong izinin aku ya. | Izinin apa? | Izinkan aku untuk tetap mencintaimu sampai mati."
-      "Cinta aku ke kamu itu seperti skripsi | Kok gitu? | Gak kelar-kelar~"
-      "Aku rela jadi Jomblo sejati, asalkan Jomblonya ditemani kamu selamanya."
-      "km itu seperti hujan yg menyejukkan hati | asekk..so pasti doonk ^_^ | tp bisa bikin aku sakit juga loh | -___-"
-      "Kalo di deket km,aku selalu jd inget bunga deh | co cwiittt banget gombal'y (ʃ⌣ƪ) | maksud aku inget bunga bangkai mbak"
-      "Kamu tau nggak bedanya bumi sama kamu? | enggak emang apa? | kalau bumi berputar pada porosnya kalau kamu berputar dalam hatiku"
-      "km tau ada berapa byk simetri lipat pada lingkaran? | tau, banyak'y tak terhingga | seperti itulah rasa galauku kepadamu :)"
-      "Boleh aku pinjem jari manis km? | ahh pasti gombal nih, mau belajar pasang cincin kan? | bukan kok aku mau ngupil"
-      "Aku disini selalu mendOakan km, dan km disana selalu mendUakan aku #bedahuruf #bedarasa"
-      "Cinta dan sayangku ke kamu itu kaya kuku. Meskipun di potong, tetep tumbuh terus"
-      "Sesuatu banget deh punya pacar kaya km | ahh biasa aja kok (ʃ⌣ƪ) | soal'y pinter banget km menguras air mataku"
-      "km tau gak kenapa pocong loncat2? | kenapa? | soal'y dia seneng liat kita udah jadian :)"
-      "Selang apa yg paling menyakitkan? | apaan? | selang sehari jadian,km udah bisa selingkuh -___-"
-      "Gara2 liat foto km,aku jd kangen | aahh masyaa? Mau gombal yaaa?? ^_^ | maksud'y jd kangen sama toilet,bikin mules sih"
-      "Tau gak kenapa kuntilanak suka nangis malem2? | emm ga tau | soal'y dia sedih liat km jomblo (-̩-̩̩-̩u-̩-̩̩-̩)"
-      "Hey kamu | Iya? | Kamu Arsitek ya? | Haa ko tahu? | Karena kamu telah membangun istana cinta di hatiku #eaaa"
-      "Sebentar lg aku 1 bulanan loh sama pacar aku | oh ya? Kapan? | hari ini kalo kamu nerima aku, berarti bulan depan... :)"
-      "Hatiku sama seperti MetroMini Sama-sama penuh sesak oleh cintamu"
-      "Sebenarnya selama ini aku pencuri. Soalnya aku suka curi-curi pandang ke kamu."
-      "Indonesia udah merdeka kan? | iya,emang udah | tapi kok perasaanku masih aja dijajah sama km? #PemberiHarapanPalsu"
-      "Kamu jago matematika ya? Boleh dong itungin detak jantungku tiap liat kamu."
-      "Mau nitip cinta sama angin ah | kok gitu? | iya,jd saat km masuk angin, saat itu pula cintaku masuk kedalam hatimu"
-      "Aku bersihin telapak kaki kamu yah | eh ngapain? | Biar surga anak-anak ku nanti bersih :D"
-      "Haii.. Cinta, lg ngapain? | aku lg kangen km nih,kalo km lg apa? | lagi cabutin bulu idung nih"
-      "Satu-satunya bahasa yang membuat aku bingung, sampai Google pun tidak bisa membantu ku adalah Bahasa Cinta darimu"
-      "Kamu tau ga bedanya kamu sama langit? kalo langit di atas kalo kamu di hati aku :*"
-      "Cintamu seperti embun yg menyejukkan hati di pagi hari, tp mudah sirna oleh terik matahari"
-      "Apa beda'y km sama nyamuk? | gak tau | kalo nyamuk ngisep darah, kalo km ngisep perasaanku"
-      "km tuh ya..yg di update status fb aja. Kapan donk update status hubungan kita?"
-      "menunggu itu pekerjaan yang membosankan | emang nungguin apa sih? | nunggu km putus sama pacar km -___-"
-      "Apa bedanya kamu sama lebah ? | Apa? | Kalo lebah membuat madu, kalo kamu membuatku rindu."
-      "Kalau kamu ajak aku melompat bareng, aku ngga bakalan mau. Mending aku lari ke bawah, bersiap menangkapmu."
-      "maaf ya aku gak bisa ngobatin kakimu yang berdarah, tapi aku bisa kok ngobatin hatimu yang terluka oleh cinta"
-      "Bapak kamu jaga warnet ya? | iya, kok tau? | tolong tanyain dong, dimana cara upload cinta ku ke hati kamu"
-      "km tau knp anak kodok loncat2? | yaah nama'y jg anak2 kan? | bukan kok, dia tuh seneng liat km putus ma pacar km"
-      "Cintamu seperti asap, pekat pada awal'y kemudian menguap begitu saja (⌣́_⌣̀)"
-      "Tau gak aku lg ada dimana? | emang dimana? | aku ada diterminal hatimu nih,lg ngetem nunggu km jomblo"
-      "Kamu bisa sopan dikit gak sih, seenaknya keluar masuk pikiranku tanpa izin."
-      "Yang layak di gantung tuh harus'y para koruptor, bukan malah perasaanku yg km gantung"
-      "Apa bedanya Monas sama Kamu? Ya bedalah, apaan emgnya? Kalo Monas milik negara, kalo kamu miliku"
-      "km punya palu ga? | buat apaan? | aku mau meruntuhkan dinding hatimu yg membeku"
-      "Naik busway..digrepe2,naik angkot.. diperkosa,jln kaki..ditabrak xenia, diem di hati km,eh ditabrak cinta deh"
-      "Tau gak di warung padang apa yang paling enak? PADANGin wajah kamu terus."
-      "Kamu bisa Cantik gitu keturunan dari Ibu / Bapak kamu bukan seeh? Kok bidadari bisa punya Ortu?!"
+      "Aku kayaknya gagal jadi penulis deh Emgnya knapa? Soalnya daritadi yg aku tulis cuma nama kamu aja :D"
+      "Hai mantan, kamu lagi belajar yah? | iya kok tau? | soalnya aku juga sama nih lagi belajar lupain km :D"
+      "Jalan-jalan kesurabaya tak lupa ke suramadu. Buat kau yang disana.. I miss you :) :D"
+      "Neng,klo kmu jd gula. Abang gk mau jd semutnya | Kok gt bang? | soalnya abang gk mau m'habiskan kmanisan wjahmu.. :D"
+      "Neng, tolong izinin aku ya. | Izinin apa? | Izinkan aku untuk tetap mencintaimu sampai mati. :D"
+      "Cinta aku ke kamu itu seperti skripsi | Kok gitu? | Gak kelar-kelar~ :D"
+      "Aku rela jadi Jomblo sejati, asalkan Jomblonya ditemani kamu selamanya. :D"
+      "km itu seperti hujan yg menyejukkan hati | asekk..so pasti doonk ^_^ | tp bisa bikin aku sakit juga loh | -___- :D"
+      "Kalo di deket km,aku selalu jd inget bunga deh | co cwiittt banget gombal'y (ʃ⌣ƪ) | maksud inget bunga bangkai mbak :D"
+      "Kamu tau g bedanya bumi sama kamu? | enggak emang apa? | kalau bumi berputar pd porosnya kalau kamu berputar di hatiku :D"
+      "km tau ada berapa byk simetri lipat pada lingkaran? | tau, banyak'y tak terhingga | seperti itulah rasa galauku ke km :D"
+      "Boleh aku pinjem jari manis km? | ahh pasti gombal nih, mau belajar pasang cincin kan? | bukan kok aku mau ngupil :D"
+      "Aku disini selalu mendOakan km, dan km disana selalu mendUakan aku #bedahuruf #bedarasa :D"
+      "Cinta dan sayangku ke kamu itu kaya kuku. Meskipun di potong, tetep tumbuh terus :D"
+      "Sesuatu banget deh punya pacar kaya km | ahh biasa aja kok (ʃ⌣ƪ) | soal'y pinter banget km menguras air mataku :D"
+      "km tau gak kenapa pocong loncat2? | kenapa? | soal'y dia seneng liat kita udah jadian :) :D"
+      "Selang apa yg paling menyakitkan? | apaan? | selang sehari jadian,km udah bisa selingkuh -___- :D"
+      "Gara2 liat foto km,aku jd kangen | aahh masaa? Mau gombal yaa?? ^_^ | maksud'y jd kangen sm toilet,bikin mules sih :D"
+      "Tau gak kenapa kuntilanak suka nangis malem2? | emm ga tau | soal'y dia sedih liat km jomblo (-̩-̩̩-̩u-̩-̩̩-̩) :D"
+      "Hey kamu | Iya? | Kamu Arsitek ya? | Haa ko tahu? | Karena kamu telah membangun istana cinta di hatiku #eaaa :D"
+      "Sebentar lg aku 1 bulanan lo sama pacar aku | oh ya? Kapan? | hari ini kl kamu nerima aku, berarti bulan depan... :) :D"
+      "Hatiku sama seperti MetroMini Sama-sama penuh sesak oleh cintamu :D"
+      "Sebenarnya selama ini aku pencuri. Soalnya aku suka curi-curi pandang ke kamu. :D"
+      "Indonesia udah merdeka kan? | iya,emang udah | tapi kok perasaanku masih aja dijajah sama km? #PHP :D"
+      "Kamu jago matematika ya? Boleh dong itungin detak jantungku tiap liat kamu. :D"
+      "Mau nitip cinta sama angin ah | kok gitu? | iya,jd saat km masuk angin, saat itu pula cintaku msuk ke hatimu :D"
+      "Aku bersihin telapak kaki kamu yah | eh ngapain? | Biar surga anak-anak ku nanti bersih :D :D"
+      "Haii.. Cinta, lg ngapain? | aku lg kangen km nih,kalo km lg apa? | lagi cabutin bulu ketek nih :D"
+      "Satu-satunya bahasa yang membuat aku bingung, sampai Google pun tidak bisa bantu adalah Bahasa Cinta darimu :D"
+      "Kamu tau ga bedanya kamu sama langit? kalo langit di atas kalo kamu di hati aku :* :D"
+      "Cintamu seperti embun yg menyejukkan hati di pagi hari, tp mudah sirna oleh terik matahari :D"
+      "Apa beda'y km sama nyamuk? | gak tau | kalo nyamuk ngisep darah, kalo km ngisep perasaanku :D"
+      "km tuh ya..yg di update status fb aja. Kapan donk update status hubungan kita? :D"
+      "menunggu itu pekerjaan yang membosankan | emang nungguin apa sih? | nunggu km putus sama pacar km -___- :D"
+      "Apa bedanya kamu sama lebah ? | Apa? | Kalo lebah membuat madu, kalo kamu membuatku rindu. :D"
+      "Kalau kamu ajak aku melompat bareng, aku g bakalan mau. Mending aku lari ke bawah, bersiap menangkapmu. :D"
+      "maaf ya aku gak bisa ngobatin kakimu yang luka, tapi aku bisa kok ngobatin hatimu yang terluka oleh cinta :D"
+      "Bapak kamu jaga warnet ya? | iya, kok tau? | tolong tanyain dong, dimana cara upload cinta ku ke hati kamu :D"
+      "km tau knp anak kodok loncat2? | yaah nama'y jg anak2 kan? | bukan, dia tuh seneng liat km putus ma pacar km :D"
+      "Cintamu seperti asap, pekat pada awal'y kemudian menguap begitu saja (⌣́_⌣̀) :D"
+      "Tau gak aku lg ada dimana? | emang dimana? | aku ada diterminal hatimu nih,lg ngetem nunggu km jomblo :D"
+      "Kamu bisa sopan dikit gak sih, seenaknya keluar masuk pikiranku tanpa izin. :D"
+      "Yang layak di gantung tuh harus'y para koruptor, bukan malah perasaanku yg km gantung :D"
+      "Apa bedanya Monas sama Kamu? Ya bedalah, apaan emgnya? Kalo Monas milik negara, kalo kamu miliku :D"
+      "km punya palu ga? | buat apaan? | aku mau meruntuhkan dinding hatimu yg membeku :D"
+      "Tau gak di warung padang apa yang paling enak? PADANGin wajah kamu terus. :D"
+      "Kamu bisa Cantik gitu keturunan dari Ibu / Bapak kamu bukan seeh? Kok bidadari bisa punya Ortu?! :D"
     )
-  gombalan_num=${#gombalan_array[*]}
-  gombalan=${gombalan_array[$((RANDOM%gombalan_num))]}  
+  twit_gombal_pilihgombalan
 
   # set tweet_status
   tweet_status="$target $gombalan"
