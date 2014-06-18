b659d812ad0ebb742098776321337d2537f4c93f
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-06-19 00:20:01 +0700

    fix: tell us when no new eps

--- plugin/kissanime.sh
+++ plugin/kissanime.sh
@@ -13,64 +13,70 @@ twit_kissanime_main() {
 
   # echo "- filter !gojibox "
   baris=$(cat "temp/gmail_kissanime" | wc -l)
-  max=$(( $baris + 1 ))
-  num=1
 
-  while [[ $num -lt $max ]]; do
-    line="`sed -n "$num"p "temp/gmail_kissanime"`"
+  if [[ "$baris" -gt 0 ]]; then
+    max=$(( $baris + 1 ))
+    num=1
+    while [[ $num -lt $max ]]; do
+      line="`sed -n "$num"p "temp/gmail_kissanime"`"
 
-    if [[ "$line" == *"Bokura wa Minna Kawaisou"* ]]; then
-      status="[kissanime] Episode terbaru Anime Bokura wa Minna Kawaisou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Detective Conan"* ]]; then
-      status="[kissanime] Episode terbaru Anime Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Detective Conan Movie 18"* ]]; then
-      status="[kissanime] Episode terbaru Anime Detective Conan Movie 18 udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Gokukoku no Brynhildr"* ]]; then
-      status="[kissanime] Episode terbaru Anime Gokukoku no Brynhildr udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Hunter x Hunter"* ]]; then
-      status="[kissanime] Episode terbaru Anime Hunter x Hunter udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Isshuukan Friends"* ]]; then
-      status="[kissanime] Episode terbaru Anime Isshuukan Friends udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Kindaichi Case Files R"* ]]; then
-      status="[kissanime] Episode terbaru Anime Kindaichi Case Files R udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Knights of Sidonia"* ]]; then
-      status="[kissanime] Episode terbaru Anime Knights of Sidonia udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Lupin III vs. Detective Conan"* ]]; then
-      status="[kissanime] Episode terbaru Anime Lupin III vs. Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Mekakucity Actors"* ]]; then
-      status="[kissanime] Episode terbaru Anime Mekakucity Actors udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Mushishi Zoku Shou"* ]]; then
-      status="[kissanime] Episode terbaru Anime Mushishi Zoku Shou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Yowamushi Pedal"* ]]; then
-      status="[kissanime] Episode terbaru Anime Yowamushi Pedal udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Barakamon"* ]]; then
-      status="[kissanime] Episode terbaru Anime Barakamon udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
-    if [[ "$line" == *"Glasslip"* ]]; then
-      status="[kissanime] Episode terbaru Anime Glasslip udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
-    fi
+      if [[ "$line" == *"Bokura wa Minna Kawaisou"* ]]; then
+        status="[kissanime] Episode terbaru Anime Bokura wa Minna Kawaisou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Detective Conan"* ]]; then
+        status="[kissanime] Episode terbaru Anime Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Detective Conan Movie 18"* ]]; then
+        status="[kissanime] Episode terbaru Anime Detective Conan Movie 18 udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Gokukoku no Brynhildr"* ]]; then
+        status="[kissanime] Episode terbaru Anime Gokukoku no Brynhildr udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Hunter x Hunter"* ]]; then
+        status="[kissanime] Episode terbaru Anime Hunter x Hunter udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Isshuukan Friends"* ]]; then
+        status="[kissanime] Episode terbaru Anime Isshuukan Friends udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Kindaichi Case Files R"* ]]; then
+        status="[kissanime] Episode terbaru Anime Kindaichi Case Files R udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Knights of Sidonia"* ]]; then
+        status="[kissanime] Episode terbaru Anime Knights of Sidonia udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Lupin III vs. Detective Conan"* ]]; then
+        status="[kissanime] Episode terbaru Anime Lupin III vs. Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Mekakucity Actors"* ]]; then
+        status="[kissanime] Episode terbaru Anime Mekakucity Actors udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Mushishi Zoku Shou"* ]]; then
+        status="[kissanime] Episode terbaru Anime Mushishi Zoku Shou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Yowamushi Pedal"* ]]; then
+        status="[kissanime] Episode terbaru Anime Yowamushi Pedal udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+      fi
+      if [[ "$line" == *"Barakamon"* ]]; then
+        status="[kissanime] Episode terbaru Anime Barakamon udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje @ulinzulfa"
+      fi
+      if [[ "$line" == *"Glasslip"* ]]; then
+        status="[kissanime] Episode terbaru Anime Glasslip udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje @ulinzulfa"
+      fi
 
-    # safety :)
-    if [[ ! -z "$status" ]]; then
-      getJam
-      echo " -[ttytter] : $status"
-      ttytter -status="$status"
-    fi
+      # safety :)
+      if [[ ! -z "$status" ]]; then
+        getJam
+        echo " -[ttytter] : $status"
+        ttytter -status="$status"
+      fi
 
-    let num++;
-  done
+      let num++;
+    done
+
+  else
+    echo "[kissanime] sepertinya nggak ada episode baru di email"
+  fi
+  
 
   echo "- done -"
   exit 1
