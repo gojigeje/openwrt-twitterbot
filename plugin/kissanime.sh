e56f692e025345d902b730af8a55d2eff61888aa
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-06-22 16:13:30 +0700

    fix: add url for each anime

--- plugin/kissanime.sh
+++ plugin/kissanime.sh
@@ -21,46 +21,49 @@ twit_kissanime_main() {
       line="`sed -n "$num"p "temp/gmail_kissanime"`"
 
       if [[ "$line" == *"Bokura wa Minna Kawaisou"* ]]; then
-        status="[kissanime] Episode terbaru Anime Bokura wa Minna Kawaisou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Bokura wa Minna Kawaisou dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fBokura-wa-Minna-Kawaisou ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Detective Conan"* ]]; then
-        status="[kissanime] Episode terbaru Anime Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Detective Conan dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fDetective-Conan ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Detective Conan Movie 18"* ]]; then
-        status="[kissanime] Episode terbaru Anime Detective Conan Movie 18 udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Detective Conan Movie 18 dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fDetective-Conan-Movie-18-The-Sniper-from-Another-Dimension ~ cc. @gojigeje @putrinawa"
       fi
       if [[ "$line" == *"Gokukoku no Brynhildr"* ]]; then
-        status="[kissanime] Episode terbaru Anime Gokukoku no Brynhildr udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Gokukoku no Brynhildr dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fGokukoku-no-Brynhildr ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Hunter x Hunter"* ]]; then
-        status="[kissanime] Episode terbaru Anime Hunter x Hunter udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Hunter x Hunter Last Mission dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fHunter-x-Hunter-The-Last-Mission ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Isshuukan Friends"* ]]; then
-        status="[kissanime] Episode terbaru Anime Isshuukan Friends udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Isshuukan Friends dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fIsshuukan-Friends ~ cc. @gojigeje @ulinzulfa"
       fi
       if [[ "$line" == *"Kindaichi Case Files R"* ]]; then
-        status="[kissanime] Episode terbaru Anime Kindaichi Case Files R udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Kindaichi Case Files R dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fKindaichi-Shounen-no-Jikenbo-Returns ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Knights of Sidonia"* ]]; then
-        status="[kissanime] Episode terbaru Anime Knights of Sidonia udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Knights of Sidonia dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fSidonia-no-Kishi ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Lupin III vs. Detective Conan"* ]]; then
-        status="[kissanime] Episode terbaru Anime Lupin III vs. Detective Conan udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Lupin III vs. Detective Conan dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fLupin-III-vs-Detective-Conan-The-Movie ~ cc. @gojigeje @putrinawa"
       fi
       if [[ "$line" == *"Mekakucity Actors"* ]]; then
-        status="[kissanime] Episode terbaru Anime Mekakucity Actors udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Mekakucity Actors dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fMekakucity-Actors ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Mushishi Zoku Shou"* ]]; then
-        status="[kissanime] Episode terbaru Anime Mushishi Zoku Shou udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Mushishi Zoku Shou dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fMushishi-Zoku-Shou ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Yowamushi Pedal"* ]]; then
-        status="[kissanime] Episode terbaru Anime Yowamushi Pedal udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje"
+        status="[kissanime] Episode baru Anime Yowamushi Pedal dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fYowamushi-Pedal ~ cc. @gojigeje"
       fi
       if [[ "$line" == *"Barakamon"* ]]; then
-        status="[kissanime] Episode terbaru Anime Barakamon udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje @ulinzulfa"
+        status="[kissanime] Episode baru Anime Barakamon dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fBarakamon ~ cc. @gojigeje @ulinzulfa"
       fi
       if [[ "$line" == *"Glasslip"* ]]; then
-        status="[kissanime] Episode terbaru Anime Glasslip udah rilis..! Liat + Download di kissanime :D ~ cc. @gojigeje @ulinzulfa"
+        status="[kissanime] Episode baru Anime Glasslip dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fGlasslip ~ cc. @gojigeje @ulinzulfa"
+      fi
+      if [[ "$line" == *"One Piece"* ]]; then
+        status="[kissanime] Episode baru Anime One Piece dh rilis..! ~ Liat + Download di http://kissanime.com/G/5726?l=http%3a%2f%2fkissanime.com%2fAnime%2fOne-Piece ~ cc. @EKOSUHARTONOcom @yunushadi"
       fi
 
       # safety :)
