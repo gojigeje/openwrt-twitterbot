b13872fa5e14d6f0eadfec4b810e4ffb1805b61c
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-06-22 16:14:27 +0700

    fix: trying to find best method to cekposting

--- plugin/cekposting.sh
+++ plugin/cekposting.sh
@@ -4,58 +4,74 @@
 
 cekposting() {
   fail=$(cat temp/*_output | grep FAILED | wc -l)
+  succ=$(cat temp/*_output | grep SUCCEEDED | wc -l)
+  echo "[cekposting] s1=$1"
 
-  if [[ "$failed" -gt 0 ]]; then
-    echo "[posting-cek-failed] -- coba reload cron + ulangi"
+  if [[ "$fail" -gt 0 ]]; then
+    echo "[cekposting-failed] -- coba reload cron + ulangi"
     /etc/init.d/cron reload
+    ntpd -p 0.openwrt.pool.ntp.org
     rm temp/*_output
     if [[ -z "$1" ]]; then
       tweet_status
       cekposting
+      echo "[cekposting] s1 kosong"
     else
       if [[ "$1" = "status" ]]; then
         failed="1"
+        echo "[cekposting] s1=status"
       fi
     fi
   else
-    if [[ -z "$1" ]]; then
-      echo "[posting-cek-success] -- siip :)"
-    else
-      if [[ "$1" = "status" ]]; then
-        failed="0"
+    if [[ "$succ" -gt 0 ]]; then
+      if [[ -z "$1" ]]; then
+        echo "[cekposting-success] -- siip :)"
+      else
+        if [[ "$1" = "status" ]]; then
+          failed="0"
+        fi
       fi
     fi
+
   fi
 }
 
-if [[ ! -z "$1" ]]; then
-  if [[ "$1" = "cron" ]]; then
-    echo "[cekposting-cron]"
-    echo -n "[cekposting-cron] - Cek failed record.. "
-    fail=$(cat /root/script/twit/temp/*_output | grep FAILED | wc -l)
-    succ=$(cat /root/script/twit/temp/*_output | grep SUCCEEDED | wc -l)
+if [[ "$1" = "cron" ]]; then
+  echo "[cekposting-cron]"
+  echo -n "[cekposting-cron] - Cek failed record.. "
+  fail=$(cat /root/script/twit/temp/*_output | grep FAILED | wc -l)
+  succ=$(cat /root/script/twit/temp/*_output | grep SUCCEEDED | wc -l)
 
-    if [[ "$fail" -gt 0 ]]; then
-      echo "$fail found.."
+  if [[ "$fail" -gt 0 ]]; then
+    echo "$fail found.."
+    echo "[cekposting-cron] - reloading crontab.. "
+    /etc/init.d/cron reload
+    ntpd -p 0.openwrt.pool.ntp.org
+    rm /root/script/twit/temp/*_output
+
+  else
+    echo "not found.."
+    echo -n "[cekposting-cron] - Cek succeeded record.. "
+    if [[ "$succ" -eq 0 ]]; then
+      echo "no recent succeeded post.."
       echo "[cekposting-cron] - reloading crontab.. "
       /etc/init.d/cron reload
+      ntpd -p 0.openwrt.pool.ntp.org
       rm /root/script/twit/temp/*_output
 
     else
-      echo "not found.."
-      echo -n "[cekposting-cron] - Cek succeeded record.. "
-      if [[ "$succ" -eq 0 ]]; then
-        echo "no recent succeeded post.."
-        echo "[cekposting-cron] - reloading crontab.. "
-        /etc/init.d/cron reload
-        rm /root/script/twit/temp/*_output
-
+      if [[ "$succ" -gt 0 ]]; then
+        echo "[cekposting-cron] - looks good.. :) "
       else
         echo "??"
         echo "something wrong here.."
+        echo "[cekposting-cron] - reloading crontab.. "
+        /etc/init.d/cron reload
+        ntpd -p 0.openwrt.pool.ntp.org
+        rm /root/script/twit/temp/*_output
       fi
     fi
-    
   fi
-  echo "[cekposting-cron] - done.. "
+  
+echo "[cekposting-cron] - done.. "
 fi
