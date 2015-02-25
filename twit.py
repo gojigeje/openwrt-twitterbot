83f999909f3dfec85efdbf9598cdf56f07f75bf7
Ghozy Arif Fajri <gojigeje@gmail.com>
2015-02-25 12:30:13 +0700

    add feature: search, auto-rt, auto-fav, trending

--- twit.py
+++ twit.py
@@ -1,36 +1,43 @@
 #!/usr/bin/python
 """
-Script tweet sederhana untuk mengakases PTT (Python Twitter Tools)
-
+  +------------------------------------------------------------------------+
+  | twit.py ~ Script sederhana untuk mengakases PTT (Python Twitter Tools) |
+  +------------------------------------------------------------------------+
+  
   Penggunaan: python twit.py [opsi] [parameter]
   
   Opsi:
-    -s ..., --status=...        isi tweet / status
-    -i ..., --image=...         path gambar
-    -d ..., --dm=...            isi direct message, harus disertai dengan -u
-    -u ..., --user=...          user target direct message
-    -f ..., --follow=...        follow user
-    -l ..., --leave=...         unfollow user
-            --follower=...      cek apakah user merupakan follower dari bot
-            --following=...     cek apakah bot memfollow user
-    -h, --help                  tampilkan bantuan
-  
-  Contoh:
-    twit.py -s "halo dunia!"                    posting tweet "halo dunia!" ke twitter
-    twit.py -i "gbr.jpg"                        posting gambar "gbr.jpg" ke twitter
-    twit.py -s "ini gambarku" -i "gbr.jpg"      posting tweet "ini gambarku" dan 
-                                                    disertai gambar "gambar.jpg" ke twitter
-    twit.py -u "gojigeje" -d "halo masbro!"     kirim DM ke @gojigeje, isinya "halo masbro!"
+    -s ..., --status=[isi]          isi tweet / status
+    -i ..., --image=[path]          path gambar
+    -r ..., --reply=[id]            id tweet yang akan direply, harus disertai dengan -s
+                                      sebagai isi dari reply nya
+    -d ..., --dm=[isi]              isi direct message, harus disertai dengan -u
+    -u ..., --user=[user]           [user] target direct message
 
-  NB:
-    Isi parameter image bisa berupa relative path atau absolute path, sangat disarankan
-    untuk mengisi parameter image dengan absolute path gambar agar meminimalisir eror. 
-    Misalnya:   twit.py -i "/home/goji/Picture/image.jpg"
+    -f ..., --follow=[user]         follow [user]
+    -l ..., --leave=[user]          unfollow [user]
+            --follower=[user]       cek apakah [user] merupakan follower dari bot
+            --following=[user]      cek apakah bot memfollow [user]
+
+            --search=[query]        search tweet yang sesuai dengan [query]
+            --auto-rt=[query]       otomatis RT tweet yang sesuai dengan [query]
+            --auto-fav=[query]      otomatis FAV tweet yang sesuai dengan [query]
+            --limit=[1-100]         jumlah tweet yang akan diproses dalam 'search' dan 'auto-*'
+            --lang=[code]           bahasa tweet yang akan diproses dalam 'search' dan 'auto-*'
+                                      [code] mengacu pada 'ISO 639-1' / kode bahasa 2 huruf
+
+            --trending=[code]       tampilkan 10 trending topic di lokasi [code]
+                                      [code] mengacu pada WOEID / 'Where On Eath ID'
+
+    -h, --help                      tampilkan bantuan
+  
+  Panduan lebih lengkap bisa dibaca di:
+    https://github.com/gojigeje/openwrt-twitterbot/blob/master/README_twit.py.md
 """
 __author__ = "Ghozy Arif Fajri <gojigeje@gmail.com>"
 __contact__ = "@gojigeje on twitter"
-__version__ = "$Revision: 1.1 $"
-__date__ = "$Date: 2015/02/18 12:53 WIB $"
+__version__ = "$Revision: 1.2 $"
+__date__ = "$Date: 2015/02/25 12:09 WIB $"
 __copyright__ = "Copyright (c) Ghozy Arif Fajri"
 __license__ = "MIT"
 
@@ -39,7 +46,7 @@ import sys
 import getopt
 
 # variabel global
-status = imagepath = direct = user = follow = leave = tipe = target = ""
+status = imagepath = direct = user = follow = leave = tipe = target = reply = query = limit = lang = woeid = ""
 
 # username akun bot, tanpa @
 botUsername = "gojibox"
@@ -56,20 +63,33 @@ def usage():
 
 def proses():
 
-    if status or imagepath:
+    if status or imagepath or reply:
         if direct or user or follow or leave or tipe:
-            if status: print("Error: opsi -s hanya bisa digunakan dengan opsi -i saja")
-            else: print("Error: opsi -i hanya bisa digunakan dengan opsi -s saja")
+            if status:
+                print("Error: opsi -s hanya bisa digunakan dengan opsi -i saja")
+            elif imagepath: 
+                print("Error: opsi -i hanya bisa digunakan dengan opsi -s saja")
+            else: 
+                print("Error: opsi -r hanya bisa digunakan dengan opsi -s saja")
         else:
-            if status and imagepath:
+            if status and imagepath and reply:
+                print("Error: opsi -r hanya bisa digunakan dengan opsi -s saja")
+            elif status and imagepath and not reply:
                 # print("twit status: "+status+", dengan gambar: "+imagepath)
                 post_image(status,imagepath)
-            elif status and not imagepath:
+            elif status and reply:
+                # print("twit reply ke : "+reply+", dengan status : "+status)
+                post_reply(reply,status)
+            elif imagepath and reply:
+                print("Error: opsi -r hanya bisa digunakan dengan opsi -s saja")
+            elif status and not imagepath and not reply:
                 # print("twit status aja: "+status)
                 post_status(status)
-            elif imagepath and not status:
+            elif imagepath and not status and not reply:
                 # print("twit gambar aja: "+imagepath)
                 post_image("",imagepath)
+            elif reply and not imagepath or status:
+                print("Error: opsi -r harus digunakan dengan opsi -s")
 
     elif direct or user:
         if status or imagepath or follow or leave or tipe:
@@ -100,10 +120,22 @@ def proses():
             # print("cek apakah "+target+" follower dari bot")
             isfollower = t.friendships.show(source_screen_name=botUsername, target_screen_name=target)["relationship"]["target"]["following"]
             print(isfollower)
-        else:
+        elif tipe == "following":
             # print("cek apakah bot following "+target)
             isfollowing = t.friendships.show(source_screen_name=target, target_screen_name=botUsername)["relationship"]["target"]["following"]
             print(isfollowing)
+        elif tipe == "search":
+            # print("Search tweets matching : '"+query+"'")
+            post_search(query,limit,lang)
+        elif tipe == "autort":
+            # print("Auto RT tweets matching : '"+query+"'")
+            auto_rt(query,limit,lang)
+        elif tipe == "autofav":
+            # print("Auto FAV tweets matching : '"+query+"'")
+            auto_fav(query,limit,lang)
+        elif tipe == "trending":
+            # print("Auto FAV tweets matching : '"+query+"'")
+            get_trends(woeid)
 
     else:
         usage()
@@ -111,6 +143,10 @@ def proses():
 def post_status(st):
     t.statuses.update(status=st)
     # print("upload status: "+st)
+
+def post_reply(twid,st):
+    t.statuses.update(in_reply_to_status_id=twid,status=st)
+    # print("upload reply ke : "+twid+", status : "+st)
 
 def post_image(st,im):
     if not st:
@@ -130,17 +166,71 @@ def post_dm(u,dm):
         user=u,
         text=dm)
 
+def search_tweets(q, count, lang, result_type="recent"):
+    # print("q: "+q)
+    # print("lang: "+lang)
+    # print("counts: "+str(count))
+    # print("result_type: "+str(result_type))
+    return t.search.tweets(q=q, result_type=result_type, count=count, lang=lang)
+
+def post_search(q,limit,lang):
+    result = search_tweets(q,limit,lang)
+    for tweet in result["statuses"]:
+        print("%s" % (tweet["id_str"]+" @"+tweet["user"]["screen_name"]+" "+tweet["text"]).encode("utf-8").replace("\n", " "))
+
+def auto_rt(q,limit,lang):
+    result = search_tweets(q,limit,lang)
+
+    for tweet in reversed(result["statuses"]):
+        try:
+            # jangan rt twit sendiri
+            if tweet["user"]["screen_name"] == botUsername:
+                continue
+
+            result = t.statuses.retweet(id=tweet["id"])
+            print("[auto-rt] %s" % ("@"+tweet["user"]["screen_name"]+" "+tweet["text"]).encode("utf-8").replace("\n", " "))
+
+        # kalau tweet sudah pernah di rt, print error
+        except TwitterHTTPError as e:
+            print("error: %s" % (str(e)))
+
+def auto_fav(q,limit,lang):
+    result = search_tweets(q,limit,lang)
+
+    for tweet in reversed(result["statuses"]):
+        try:
+            # jangan fav twit sendiri
+            if tweet["user"]["screen_name"] == botUsername:
+                continue
+
+            result = t.favorites.create(_id=tweet["id"])
+            print("[auto-fav] %s" % ("@"+tweet["user"]["screen_name"]+" "+tweet["text"]).encode("utf-8").replace("\n", " "))
+
+        # kalau tweet sudah pernah di fav, print error
+        except TwitterHTTPError as e:
+            print("error: %s" % (str(e)))
+
+def get_trends(woeid):
+    result = t.trends.place(_id=woeid)
+
+    location = result[0]["locations"]
+    print(location[0]["name"]+" trends:")
+
+    trending = result[0]["trends"]
+    for tweet in trending:
+        print(tweet["name"]) 
+
 def main(argv):
     if not argv:
         usage()
     else:
         try:
-            opts, args = getopt.getopt(argv, "hs:i:d:u:f:l:", ["help", "status=", "image=", "dm=", "user=", "follow=", "leave=", "follower=", "following="])
+            opts, args = getopt.getopt(argv, "hs:i:r:d:u:f:l:", ["help", "status=", "image=", "reply=", "dm=", "user=", "follow=", "leave=", "follower=", "following=", "search=", "limit=", "lang=", "auto-rt=", "auto-fav=", "trending="])
         except getopt.GetoptError:
             usage()
             sys.exit()
         for opt, arg in opts:
-            global status, imagepath, direct, user, follow, leave, target, tipe
+            global status, imagepath, direct, user, follow, leave, target, tipe, reply, query, limit, lang, woeid
 
             if opt in ("-h", "--help"):
                 usage()
@@ -149,6 +239,8 @@ def main(argv):
                 status = arg
             elif opt in ("-i", "--image"):
                 imagepath = arg
+            elif opt in ("-r", "--reply"):
+                reply = arg
             elif opt in ("-d", "--dm"):
                 direct = arg
             elif opt in ("-u", "--user"):
@@ -163,6 +255,22 @@ def main(argv):
             elif opt in ("--following"):
                 target = arg
                 tipe = "following"
+            elif opt in ("--search"):
+                query = arg
+                tipe = "search"
+            elif opt in ("--limit"):
+                limit = arg
+            elif opt in ("--lang"):
+                lang = arg
+            elif opt in ("--auto-rt"):
+                query = arg
+                tipe = "autort"
+            elif opt in ("--auto-fav"):
+                query = arg
+                tipe = "autofav"
+            elif opt in ("--trending"):
+                woeid = arg
+                tipe = "trending"
 
         proses()
     
